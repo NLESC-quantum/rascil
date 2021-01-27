65c608e2753b3e0f05e01278de8d1e3e1c5103cb
gabicca <gabriella.hodosan@stfc.ac.uk>
2021-01-27 17:20:33 +0000

    SIM-706: add bundle_gemfile variable to variables + remove bash script

--- build_bot.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-git clone https://github.com/dependabot/dependabot-script.git &
-PROC_ID=$!
-
-wait $PROC_ID
-
-BUNDLE_GEMFILE="./dependabot-script/Gemfile" bundle install -j $(nproc) --path vendor

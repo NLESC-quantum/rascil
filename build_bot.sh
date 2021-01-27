7b80609a34633a4420f2d9b8214b5e553e608e77
gabicca <gabriella.hodosan@stfc.ac.uk>
2021-01-27 17:09:20 +0000

    SIM-706: add wait to sh

--- build_bot.sh
+++ build_bot.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/dependabot/dependabot-script.git
+git clone https://github.com/dependabot/dependabot-script.git &
+PROC_ID=$!
+
+wait $PROC_ID
 
 BUNDLE_GEMFILE="/dependabot-script/Gemfile" bundle install -j $(nproc) --path vendor

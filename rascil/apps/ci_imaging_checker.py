109f7cd7d4a8c92ed0e82892fd80b1b786b1eab9
sddyates <sdaley@mrao.cam.ac.uk>
2021-02-18 09:28:28 +0000

    Added image statistics function as well as a function for plotting histogram with fitted Gaussian.

--- rascil/apps/ci_imaging_checker.py
+++ rascil/apps/ci_imaging_checker.py
@@ -9,12 +9,14 @@ import os
 import sys
 
 import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
 
 matplotlib.use('Agg')
 import pandas as pd
 import bdsf
 import astropy.units as u
+from astropy import modeling
 from astropy.coordinates import SkyCoord
 from rascil.data_models.polarisation import PolarisationFrame
 from rascil.processing_components import create_low_test_skycomponents_from_gleam
@@ -34,7 +36,7 @@ log.addHandler(logging.StreamHandler(sys.stdout))
 def cli_parser():
     """ Get a command line parser and populate it with arguments
 
-    :param parser: argparse    
+    :param parser: argparse
     :return: CLI parser argparse
     """
     parser = argparse.ArgumentParser(description='RASCIL continuum image checker')
@@ -59,92 +61,161 @@ def cli_parser():
                         help='Name of output source file')
     parser.add_argument('--logfile', type=str, default=None,
                         help='Name of output log file')
-    
+
     return parser
 
 
 def analyze_image(args):
     """
     Main analysis routine.
-    
+
     :param args: argparse with appropriate arguments
 
     :return:    A list of sources and their matches to original sources
 
     """
-    
+
     cwd = os.getcwd()
-    
+
     if args.ingest_fitsname is None:
         raise FileNotFoundError("Input FITS file name must be specified")
-    
+
     if args.logfile is None:
         logfile = args.ingest_fitsname.replace('.fits', ".log")
     else:
         logfile = args.logfile
-    
+
     if args.source_file is None:
         source_file = args.ingest_fitsname.replace('.fits', ".csv")
     else:
         source_file = args.source_file
-    
+
     def init_logging():
         logging.basicConfig(filename=logfile,
                             filemode='a',
                             format='%(asctime)s.%(msecs)d %(name)s %(levelname)s %(message)s',
                             datefmt='%d/%m/%Y %I:%M:%S %p',
                             level=logging.INFO)
-    
+
     init_logging()
-    
+
     log.info("\nRASCIL Continuum Imagine Checker\n")
-    
+
     starttime = datetime.datetime.now()
     log.info("Started : {}".format(starttime))
     log.info("Writing log to {}".format(logfile))
-    
+
     input_image = args.ingest_fitsname
-    
+
     im = import_image_from_fits(args.ingest_fitsname)
-    
+
     beam_maj = args.finder_beam_maj
     beam_min = args.finder_beam_min
     beam_pos_angle = args.finder_beam_pos_angle
     beam_info = (beam_maj, beam_min, beam_pos_angle)
-    
+
     th_isl = args.finder_th_isl
     th_pix = args.finder_th_pix
-    
+
     # Adjust the best restoring beam configuration-- not sure about this yet
     # factor = 1/(npixels/512) **2
     # beam_info = (beam_info[0]* factor, beam_info[1] * factor, beam_info[2])
-    
+
     freq = im.frequency.data[0]
     log.info("Use restoring beam: {}".format(beam_info))
     log.info("Use threshold: {}".format(th_isl, th_pix))
-    
+
     ci_checker(input_image, beam_info, source_file, th_isl, th_pix)
     out = create_source_to_skycomponent(source_file, freq)
-    
+
     if args.apply_primary:
         telescope = args.telescope_model
         out = add_primary_beam(input_image, out, telescope)
-    
+
     match_sep = args.match_sep
     results = check_source(im.image_acc.phasecentre, freq, out, match_sep)
-    
+
     log.info("Resulting list of items {}".format(results))
-    
+
     log.info("Started  : {}".format(starttime))
     log.info("Finished : {}".format(datetime.datetime.now()))
-    
+
     return results
+
+
+def bdsf_qa_image(im_data):
+    """Assess the quality of an image
+
+    Set of statistics of an image; max, min, maxabs, rms, sum, medianabs, medianabsdevmedian, median
+
+    :param im_data: image data
+    :return: None
+    """
+
+    image_stats = {
+        'shape': str(im_data.data.shape),
+        'max': np.max(im_data),
+        'min': np.min(im_data),
+        'maxabs': np.max(np.abs(im_data)),
+        'rms': np.std(im_data),
+        'sum': np.sum(im_data),
+        'medianabs': np.median(np.abs(im_data)),
+        'medianabsdevmedian': np.median(np.abs(im_data - np.median(im_data))),
+        'median': np.median(im_data),
+        'mean': np.mean(im_data)
+    }
+
+    log.info("QA of image:")
+    for item in image_stats.items():
+        log.info("    {}".format(item))
+
+    return image_stats
+
+
+def ci_checker_diagnostics(img):
+    """
+    Log and plot diagnostics from the ci_checker.
+
+    :param img: pybdsf image object
+
+    :return None
+    """
+
+    im_data = img.resid_gaus_arr
+
+    image_stats = bdsf_qa_image(im_data)
+
+    n, bins, _ = plt.hist(im_data.ravel(), bins=1000, density=False)
+
+    fitter = modeling.fitting.LevMarLSQFitter()
+    model = modeling.models.Gaussian1D(
+        amplitude=image_stats['max'],
+        mean=image_stats['mean'],
+        stddev=image_stats['rms']
+    )
+
+    # "bins" are the bin edge points, so need the mid points.
+    mid_points = bins[:-1] + 0.5*abs(bins[1:] - bins[:-1])
+
+    fitted_model = fitter(model, mid_points, n)
+
+    plt.plot(mid_points, fitted_model(mid_points))
+    plt.yscale("symlog")
+
+    diagnostics_dir = "./diagnostics"
+    if not os.path.exists(diagnostics_dir):
+        os.makedirs(diagnostics_dir)
+
+    plt.savefig(diagnostics_dir + "/hist_continuum_imaging_residuals.png")
+    plt.close()
+
+    return
 
 
 def ci_checker(input_image, beam_info, source_file, th_isl, th_pix):
     """
     PyBDSF-based source finder
-    
+
     :param input_image
     :param beam_info : Size of restoring beam as (bmaj, bmin, pos_angle)
     :param source_file : Output file name of the source list
@@ -153,16 +224,18 @@ def ci_checker(input_image, beam_info, source_file, th_isl, th_pix):
 
     : return None
     """
-    
+
     # Process image.
     img = bdsf.process_image(input_image, beam=beam_info, thresh_isl=th_isl, thresh_pix=th_pix)
-    
+
     # Write the source catalog and the residual image.
     img.write_catalog(outfile=source_file, format='csv', catalog_type='srl', clobber=True)
     img.write_catalog(format='fits', catalog_type='srl', clobber=True)
     img.export_image(img_type='gaus_resid', clobber=True)
     # img.export_image(img_type='gaus_model', clobber=True)
-    
+
+    ci_checker_diagnostics(img)
+
     return
 
 
@@ -175,7 +248,7 @@ def create_source_to_skycomponent(source_file, freq):
 
     :return comp: List of skycomponents
     """
-    
+
     data = pd.read_csv(source_file, sep=r'\s*,\s*', skiprows=5)
     comp = []
     for i, row in data.iterrows():
@@ -184,7 +257,7 @@ def create_source_to_skycomponent(source_file, freq):
         f = row['Total_flux']
         comp.append(create_skycomponent(direction=direc, flux=np.array([[f]]), frequency=np.array([freq]),
                                         polarisation_frame=PolarisationFrame('stokesI')))
-    
+
     return comp
 
 
@@ -196,35 +269,35 @@ def add_primary_beam(input_image, comp, telescope):
     :param comp: Source list in skycomponents format
     :param telescope: Telescope model to use, i.e. MID or LOW
 
-    :return pbcomp: Corrected lists of skycomponents. 
+    :return pbcomp: Corrected lists of skycomponents.
 
     """
     image = import_image_from_fits(input_image, fixpol=True)
     pb = create_pb(image, telescope=telescope, use_local=False)
     pbcomp = apply_beam_to_skycomponent(comp, pb)
-    
+
     return pbcomp
 
 
 def check_source(centre, freq, comp, match_sep):
     """
     Check the difference between output sources and input.
-    
+
     :param centre: Phase centre of image
     :param freq: Frequency or list of frequencies.
     :param comp: Output source list in skycomponent format
     :param match_sep: The criteria for maximum separation
- 
+
     :return matches: List of matched skycomponents
- 
+
     """
     orig = create_low_test_skycomponents_from_gleam(flux_limit=1.0, phasecentre=centre,
                                                     frequency=np.array([freq]),
                                                     polarisation_frame=PolarisationFrame('stokesI'), radius=0.5)
-    
+
     # separations = find_separation_skycomponents(comp, orig)
     matches = find_skycomponent_matches(comp, orig, tol=match_sep)
-    
+
     return matches
 
 if __name__ == "__main__":
@@ -233,4 +306,3 @@ if __name__ == "__main__":
     parser = cli_parser()
     args = parser.parse_args()
     analyze_image(args)
-

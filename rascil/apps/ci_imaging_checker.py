50d040b1429852100f752b6251890d1adaa661a9
gabicca <gabriella.hodosan@stfc.ac.uk>
2021-03-26 14:27:10 +0000

    SIM-803: implement reading args from file + update docs

--- rascil/apps/ci_imaging_checker.py
+++ rascil/apps/ci_imaging_checker.py
@@ -55,7 +55,8 @@ def cli_parser():
     :param parser: argparse
     :return: CLI parser argparse
     """
-    parser = argparse.ArgumentParser(description="RASCIL continuum imaging checker")
+    parser = argparse.ArgumentParser(description="RASCIL continuum imaging checker",
+                                     fromfile_prefix_chars="@")
     parser.add_argument(
         "--ingest_fitsname_restored",
         type=str,

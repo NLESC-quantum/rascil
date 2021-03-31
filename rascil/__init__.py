b034024d97af3c4729cee0ccaa3b963275bfaaf6
Tim Cornwell <realtimcornwell@gmail.com>
2021-03-31 09:51:40 +0100

    Remove .apps from global import

--- rascil/__init__.py
+++ rascil/__init__.py
@@ -1,7 +1,6 @@
 from . import data_models
 from . import processing_components
 from . import workflows
-from . import apps
 from . import phyconst
 
 from .processing_components.util.installation_checks import check_data_directory

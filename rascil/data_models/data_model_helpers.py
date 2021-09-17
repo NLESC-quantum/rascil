b303cabb367d0f54ae0bd535f7ff605ca8b69629
Tim Cornwell <realtimcornwell@gmail.com>
2021-09-17 10:10:20 +0100

    ORC-1037: First round of removing fossils

--- rascil/data_models/data_model_helpers.py
+++ rascil/data_models/data_model_helpers.py
@@ -791,11 +791,7 @@ def convert_hdf_to_image(f):
     ):
         polarisation_frame = PolarisationFrame(f.attrs["polarisation_frame"])
         wcs = WCS(f.attrs["wcs"])
-        frequency = numpy.array(f.attrs["frequency"])
         data = numpy.array(f["data"])
-        s = f.attrs["phasecentre_coords"].split()
-        ss = [float(s[0]), float(s[1])] * u.deg
-        phasecentre = SkyCoord(ra=ss[0], dec=ss[1], frame=f.attrs["phasecentre_frame"])
         im = Image(data=data, polarisation_frame=polarisation_frame, wcs=wcs)
         return im
     else:
@@ -995,7 +991,7 @@ def export_griddata_to_hdf5(gd, filename):
             f.attrs["number_data_models"] = len(gd)
             for i, v in enumerate(gd):
                 vf = f.create_group("GridData%d" % i)
-                convert_image_to_hdf(v, vf)
+                convert_griddata_to_hdf(v, vf)
         else:
             f.attrs["number_data_models"] = 1
             vf = f.create_group("GridData%d" % 0)

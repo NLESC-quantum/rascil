00495ee8b808a899e61a30f62ba486092d64b67b
Tim Cornwell <realtimcornwell@gmail.com>
2021-10-21 16:35:34 +0100

    ORC-1037: Upgrade flagging functions to run

--- rascil/processing_components/flagging/operations.py
+++ rascil/processing_components/flagging/operations.py
@@ -7,7 +7,7 @@ order in the WCS is reversed so the grid_WCS describes UU, VV, WW, STOKES, FREQ 
 
 """
 
-__all__ = ["flagging_blockvisibility", "flagging_blockvisibility_with_bl"]
+__all__ = ["flagging_blockvisibility"]
 
 import logging
 
@@ -19,64 +19,23 @@ from rascil.data_models.memory_data_models import BlockVisibility, FlagTable
 log = logging.getLogger("rascil-logger")
 
 
-def flagging_blockvisibility(bvis, antenna=[], channel=[], polarization=[]):
+def flagging_blockvisibility(
+    bvis, baselines=[], antennas=[], channels=[], polarisations=[]
+):
     """Flagging BlockVisibility
 
     :param bvis: BlockVisibility
-    :param antenna: The list of antenna number
-    :param channel: The list of Channel number
-    :param polarization:
+    :param antennas: The list of antenna number
+    :param channels: The list of Channel number
+    :param polarisations: The list of polarisations
     :return: BlockVisibility
     """
-
-    # assert isinstance(bvis, BlockVisibility), bvis
-    # assert isinstance(antenna, list)
-    if len(antenna) == 0:
-        flagged_baselines = list(range(len(bvis.baselines)))
-    else:
-        flagged_baselines = []
-        for ibaseline, (a1, a2) in enumerate(bvis.baselines.data):
-            if a1 in antenna:
-                flagged_baselines.append(ibaseline)
-            if a2 in antenna:
-                flagged_baselines.append(ibaseline)
-
-    if len(channel) == 0 and len(polarization) == 0:
-        for ibaseline in flagged_baselines:
+    for channel in channels:
+        bvis["flags"].data[..., channel, :] = 1
+    for pol in polarisations:
+        bvis["flags"].data[..., pol] = 1
+    for ibaseline, (a1, a2) in enumerate(bvis.baselines.data):
+        if a1 in antennas or a2 in antennas:
             bvis["flags"].data[:, ibaseline, ...] = 1
-            bvis["flags"].data[:, ibaseline, ...] = 1
-    elif len(channel) == 0:
-        for ibaseline in flagged_baselines:
-            for pol in polarization:
-                bvis["flags"].data[:, ibaseline, ..., pol] = 1
-                bvis["flags"].data[:, ibaseline, ..., pol] = 1
-    elif len(polarization) == 0:
-        for ibaseline in flagged_baselines:
-            for ch in channel:
-                bvis["flags"].data[:, ibaseline, ch, :] = 1
-                bvis["flags"].data[:, ibaseline, ch, :] = 1
-    else:
-        for ibaseline in flagged_baselines:
-            for ch in channel:
-                for pol in polarization:
-                    bvis["flags"].data[:, ibaseline, ch, pol] = 1
-                    bvis["flags"].data[:, ibaseline, ch, pol] = 1
 
-    return bvis
-
-
-def flagging_blockvisibility_with_bl(bvis, baseline=[]):
-    """Flagging BlockVisibility with Baseline (nant, nant, channel, pol)
-
-    :param bvis: BlockVisibility
-    :param polarization:
-    :return: BlockVisibility
-    """
-
-    # assert isinstance(bvis, BlockVisibility), bvis
-    # assert isinstance(baseline, list)
-    if len(baseline) == 0:
-        for ant1, ant2 in baseline:
-            ibaseline = bvis.baselines.get_loc((ant1, ant2))
-            bvis["flags"].data[:, ibaseline, :, :] = 1
     return bvis
